3ddb1125da24eb77605929e99861a9684311d003
Jayanth Kumar <jayanthchalla12345@gmail.com>
2025-08-25 19:52:16 +0530

    Animation added

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,18 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      keyframes: {
+        "zoom-in-zoom-out": {
+          "0%": { transform: "scale(1)" },
+          "50%": { transform: "scale(1.07)" },
+          "100%": { transform: "scale(1)" },
+        },
+      },
+      animation: {
+        "pulse-zoom": "zoom-in-zoom-out 4s ease-in-out infinite",
+      },
+    },
   },
   plugins: [],
 };
