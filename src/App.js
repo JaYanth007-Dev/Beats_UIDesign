0424a64aaf2c3e1af01d9b68e1c2a22670128781
Jayanth Kumar <jayanthchalla12345@gmail.com>
2025-08-25 22:36:31 +0530

    Routes Added

--- src/App.js
+++ src/App.js
@@ -2,31 +2,62 @@ import './App.css';
 import Header from './components/Header'
 import Hero from './components/Hero'
 import AOS from "aos";
-import "aos/dist/aos.css"; 
+import "aos/dist/aos.css";
 import CompanyLogos from "./components/CompanyLogos";
 import Specifications from "./components/Specifications";
 import CaseSection from "./components/CaseSection";
-import BuyNowSection from "./components/BuyNowSection";
-import ProductSelection from "./components/ProductSelection";
+import BuyNowSection from "./components/BuyNowSection";import ProductSelection from "./components/ProductSelection";
+import { Routes, Route } from "react-router-dom";
 
 import Footer from "./components/Footer";
 import { useEffect } from 'react';
+import ProductsPage from './components/ProductPage';
+import CartPage from './components/CartPage';
+import PaymentPage from './components/PaymentPage';
+import LoginPage from './components/LoginAndSignup';
 function App() {
-   useEffect(() => {
-     AOS.init({
-       duration: 1200, 
-     });
-   }, []);
+  useEffect(() => {
+    AOS.init({
+      duration: 1200,
+    });
+  }, []);
   return (
-    <div className="min-h-screen bg-[#0F0F10] text-white">
-      <Header />
-      <Hero />
-      <CompanyLogos />
-      <Specifications />
-      <CaseSection />
-      <BuyNowSection/>
-      <Footer />
-    </div>
+      <div className="min-h-screen bg-[#0F0F10] text-white">
+        <Header />
+
+        <Routes>
+          <Route
+            path="/"
+            element={
+              <>
+                <Hero />
+                <CompanyLogos />
+                <Specifications />
+                <CaseSection />
+                <BuyNowSection />
+              </>
+            }
+          />
+
+          <Route path="/products" element={<ProductsPage />} />
+
+          <Route
+            path="/cart"
+            element={<CartPage/>}
+          />
+
+          <Route
+            path="/login"
+            element={<LoginPage/> }
+          />
+          <Route
+            path="/payments"
+            element={ <PaymentPage/> }
+          />
+        </Routes>
+
+        <Footer />
+      </div>
   );
 }
 
